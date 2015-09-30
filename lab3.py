fc9f9cb4c5605dfe3f3ea5447b6495302b4f01b7
Paula Justi <paulinhajusti@hotmail.com>
2015-09-30 16:05:47 +0000

    alteration of print and else structure

--- lab3.py
+++ lab3.py
@@ -3,11 +3,10 @@ phrase = input("Enter a string: ")
 #helps in recognition of letters uppercase and lowercase
 phrase = phrase.casefold()
 
-
 rev_str = reversed(phrase)
 
 #method to revert the phrase and compare
 if list(phrase) == list(rev_str):
    print("It is palindrome")
 else:
-   print("It is not palindrome")
+   print("It is not palindrome. The phrase is not the same if you reverse it")
